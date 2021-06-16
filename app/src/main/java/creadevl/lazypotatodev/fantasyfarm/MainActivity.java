e472010db9f0c35ada911db93b5d04470b9527e4
polygonnedpotato <cuteiethecorgi@gmail.com>
2021-06-16 16:21:23 -0700

    Initial commit

---
+++ app/src/main/java/creadevl/lazypotatodev/fantasyfarm/MainActivity.java
@@ -0,0 +1,14 @@
+package creadevl.lazypotatodev.fantasyfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
